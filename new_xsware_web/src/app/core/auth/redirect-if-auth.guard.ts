356ab201dc1fefbdd9dbf54c808fff7d08aea199
Krzysztof Skarbowski <krzysztof.skarbowski@gmail.com>
2026-02-23 14:27:06 +0100

    feat: redirect if authenticated and logi/register page

---
+++ new_xsware_web/src/app/core/auth/redirect-if-auth.guard.ts
@@ -0,0 +1,22 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { SessionStore } from './session.store';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { filter, map, take } from 'rxjs/operators';
+
+export const redirectIfAuthGuard: CanActivateFn = () => {
+  
+  const session = inject(SessionStore);
+  const router = inject(Router);
+
+  return toObservable(session.initialized).pipe(
+    filter((init) => init === true),
+    take(1),
+    map(() => {
+      if (session.isAuthenticated()) {
+        return router.createUrlTree(['/profile']);
+      }
+      return true;
+    }),
+  );
+};
